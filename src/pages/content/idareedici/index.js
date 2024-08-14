cbab5c8d5085fd1ec3811f329e454009a686a0ec
alimahmudlu <alimahmudlu@gmail.com>
2024-08-14 11:17:31 +0400

    update

---
+++ src/pages/content/idareedici/index.js
@@ -0,0 +1,129 @@
+import {MainLayout} from "@/admin/components/layouts";
+import DashboardItem from "@/admin/components/ui/DashboardItem";
+
+export default function Index(props) {
+    return (
+        <>
+            <div className='row gap-y-[16px]'>
+                <div className='col-lg-4'>
+                    <DashboardItem
+                        header='Pages'
+                        description='Pages description'
+                        path='/content/idareedici/pages'
+                        list={[
+                            {
+                                name: 'List',
+                                path: '/content/idareedici/pages',
+                            },
+                            {
+                                name: 'Add',
+                                path: '/content/idareedici/pages/create',
+                            },
+                            {
+                                name: 'Relations',
+                                path: '/content/idareedici/pages/relations',
+                            }
+                        ]}
+                        length={12}
+                    />
+                </div>
+                <div className='col-lg-4'>
+                    <DashboardItem
+                        header='Posts'
+                        description='Posts description'
+                        path='/content/idareedici/posts'
+                        list={[
+                            {
+                                name: 'List',
+                                path: '/content/idareedici/posts',
+                            },
+                            {
+                                name: 'Add',
+                                path: '/content/idareedici/posts/create',
+                            }
+                        ]}
+                        length={12}
+                    />
+                </div>
+                <div className='col-lg-4'>
+                    <DashboardItem
+                        header='Languages'
+                        description='Languages description'
+                        path='/content/idareedici/languages'
+                        list={[
+                            {
+                                name: 'List',
+                                path: '/content/idareedici/languages',
+                            },
+                            {
+                                name: 'Add',
+                                path: '/content/idareedici/languages/create',
+                            }
+                        ]}
+                        length={12}
+                    />
+                </div>
+                <div className='col-lg-4'>
+                    <DashboardItem
+                        header='Data types'
+                        description='Data types description'
+                        path='/content/idareedici/data-types'
+                        list={[
+                            {
+                                name: 'List',
+                                path: '/content/idareedici/data-types',
+                            },
+                            {
+                                name: 'Add',
+                                path: '/content/idareedici/data-types/create',
+                            }
+                        ]}
+                        length={12}
+                    />
+                </div>
+                <div className='col-lg-4'>
+                    <DashboardItem
+                        header='Menus'
+                        description='Menus description'
+                        path='/content/idareedici/menus'
+                        list={[
+                            {
+                                name: 'List',
+                                path: '/content/idareedici/menus',
+                            },
+                            {
+                                name: 'Add',
+                                path: '/content/idareedici/menus/create',
+                            }
+                        ]}
+                        length={12}
+                    />
+                </div>
+                <div className='col-lg-4'>
+                    <DashboardItem
+                        header='Settings'
+                        description='Settings description'
+                        path='/content/idareedici/settings'
+                        list={[
+                            {
+                                name: 'Structure',
+                                path: '/content/idareedici/settings',
+                            }
+                        ]}
+                        length={12}
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
+
+Index.getLayout = function getLayout(page) {
+    return (
+        <>
+            <MainLayout>
+                {page}
+            </MainLayout>
+        </>
+    )
+}
---
+++ src/pages/content/idareedici/menus/index.js
@@ -0,0 +1,216 @@
+import {MainLayout} from "@/admin/components/layouts";
+import {SgPage, SgPageBody, SgPageHead} from "@/admin/components/ui/Page";
+import {SgButton} from "@/admin/components/ui/Button";
+import SgTable from "@/admin/components/ui/Table";
+import {
+    DATA_TYPE_LIST_ROUTE,
+    LANGUAGE_LIST_ROUTE, MENU_TYPE_DELETE_ROUTE, MENU_TYPE_LIST_ROUTE,
+    PAGE_LIST_ROUTE,
+    POST_DELETE_ROUTE, WIDGET_DELETE_ROUTE,
+    WIDGET_LIST_ROUTE
+} from "@/admin/configs/apiRoutes";
+import {useEffect, useState} from "react";
+import ApiService from "@/admin/services/ApiService";
+import SgButtonGroup from "@/admin/components/ui/ButtonGroup/ButtonGroup";
+import {SgPopup} from "@/admin/components/ui/Popup";
+
+export default function Index(props) {
+    const [selectedRow, setSelectedRow] = useState({});
+    const [filters, setFilters] = useState({});
+    const [languageList, setLanguageList] = useState([]);
+    const [removeItemModal, setRemoveItemModal] = useState(false);
+    const [statusOptions, setStatusOptions] = useState([
+        {
+            id: 1,
+            name: 'Active'
+        },
+        {
+            id: 0,
+            name: 'Deactive'
+        }
+    ]);
+
+    function toggleRemoveItemModal() {
+        setRemoveItemModal(!removeItemModal)
+    }
+
+    function handleRemoveItem() {
+        ApiService.delete(`${MENU_TYPE_DELETE_ROUTE}/${selectedRow.id}`).then(response => {
+            toggleRemoveItemModal()
+            setFilters(filters)
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
+    useEffect(() => {
+        ApiService.get(LANGUAGE_LIST_ROUTE).then(response => {
+            setLanguageList(response.data.data.data)
+        })
+    }, []);
+
+
+    return (
+        <>
+            <SgPage>
+                <SgPageHead
+                    header='Menus'
+                    description='View your site menus.'
+                    filter={true}
+                >
+                    <SgButton
+                        type='link'
+                        isLinked={true}
+                        to='/content/idareedici/menus/create'
+                        color='primary'
+                        size='md'
+                        icon='plus'
+                    >
+                        Add menu
+                    </SgButton>
+                </SgPageHead>
+                <SgPageBody>
+                    <SgTable
+                        tableData={{
+                            data: [
+                                {
+                                    key: 'id',
+                                    name: 'ID',
+                                    hidden: false,
+                                    cell: (row, key) => {
+                                        return (
+                                            <>
+                                                {key}
+                                            </>
+                                        )
+                                    }
+                                },
+                                {
+                                    key: 'name',
+                                    name: 'Name',
+                                    hidden: false,
+                                    cell: (row, key) => {
+                                        return (
+                                            <>
+                                                {key}
+                                            </>
+                                        )
+                                    }
+                                },
+                                {
+                                    key: 'alias',
+                                    name: 'Alias',
+                                    hidden: false,
+                                    cell: (row, key) => {
+                                        return (
+                                            <>
+                                                {key}
+                                            </>
+                                        )
+                                    }
+                                },
+                                {
+                                    key: 'id',
+                                    name: 'Menu structure',
+                                    hidden: false,
+                                    cell: (row, key) => {
+                                        return (
+                                            <>
+                                                <SgButtonGroup>
+                                                    {
+                                                        (languageList || []).map((language, index) => {
+                                                            return (
+                                                                <SgButton
+                                                                    key={index}
+                                                                    size='xs'
+                                                                    color={'primary'}
+                                                                    type='link'
+                                                                    to={`/content/idareedici/menus/structure/${key}/${language?.locale}`}
+                                                                >
+                                                                    {language?.locale}
+                                                                </SgButton>
+                                                            )
+                                                        })
+                                                    }
+                                                </SgButtonGroup>
+                                            </>
+                                        )
+                                    }
+                                },
+                                {
+                                    key: 'id',
+                                    name: 'Operation',
+                                    hidden: false,
+                                    cell: (row, key) => {
+                                        return (
+                                            <>
+                                                <SgButtonGroup>
+                                                    <SgButton
+                                                        size='xs'
+                                                        color='primary'
+                                                        type='link'
+                                                        to={`/admin/menus/edit/${key}`}
+                                                    >
+                                                        Edit
+                                                    </SgButton>
+                                                    <SgButton
+                                                        size='xs'
+                                                        color='error'
+                                                        onClick={toggleRemoveItemModal}
+                                                    >
+                                                        Remove
+                                                    </SgButton>
+                                                </SgButtonGroup>
+                                            </>
+                                        );
+                                    }
+                                }
+                            ],
+                            api: MENU_TYPE_LIST_ROUTE,
+                            filters: {filters}
+                        }}
+                        onClick={(e, row, index) => {setSelectedRow(row)}}
+                    />
+                </SgPageBody>
+                <SgPopup
+                    header='Remove Post'
+                    description='lol'
+                    size='md'
+                    setToggleModal={toggleRemoveItemModal}
+                    toggleModal={removeItemModal}
+                >
+                    <SgButtonGroup
+                        gap={true}
+                    >
+                        <SgButton
+                            size='lg'
+                            color='error'
+                            onClick={handleRemoveItem}
+                        >
+                            Remove
+                        </SgButton>
+                        <SgButton
+                            size='lg'
+                            color='primary'
+                            onClick={toggleRemoveItemModal}
+                        >
+                            Cancel
+                        </SgButton>
+                    </SgButtonGroup>
+                </SgPopup>
+            </SgPage>
+        </>
+    )
+}
+
+Index.getLayout = function getLayout(page) {
+    return (
+        <>
+            <MainLayout>
+                {page}
+            </MainLayout>
+        </>
+    )
+}
+
+
