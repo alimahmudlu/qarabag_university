30e9fc8b785b579f9d6ddf5fba134cbb449c6ac7
alimahmudlu <alimahmudlu@gmail.com>
2024-08-14 10:14:17 +0400

    update

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,3 +1,38 @@
+import {SiteLayout} from "@/components/layouts";
+import Link from "next/link";
+import GetGenerateMetadata from "@/utils/getGenerateMetadata";
+import SgSectionMainHero from "@/components/sections/MainHero";
+
 export default function Index() {
-	return <h1>ERRORRRR - Page Not Found</h1>
+	return (
+		<>
+			<GetGenerateMetadata
+				meta={{
+					title: '',
+					description: 'asas',
+				}}
+			/>
+
+			<SgSectionMainHero
+				id='mainHero'
+				inner={true}
+				header={''}
+				breadcrumb={[]}
+			/>
+
+			<div className='p-[150px]'>
+				<Link href='/'>ERROR</Link>
+			</div>
+		</>
+	)
+}
+
+Index.getLayout = function getLayout(page, menus, languages) {
+	return (
+		<>
+			<SiteLayout menus={menus} languages={languages}>
+				{page}
+			</SiteLayout>
+		</>
+	)
 }
