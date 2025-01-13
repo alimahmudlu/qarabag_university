ea5f5967c991225667484e68747ec4a072da93a5
alimahmudlu <alimahmudlu@gmail.com>
2025-01-13 15:31:36 +0400

    update

--- src/pages/search.js
+++ src/pages/search.js
@@ -4,9 +4,88 @@ import SgPageError from "@/components/pages/ErrorPage";
 import SgSectionSearchSection from "@/components/sections/SearchSection";
 import ApiService from "@/services/ApiService";
 import {SEARCH_CONTENTS_ROUTE} from "@/configs/apiRoutes";
+import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
+import {changeData} from "@/utils/changeData";
 
 export default function Index(props) {
 	const { searchData } = props;
+	const router = useRouter()
+	const { query } = useRouter()
+	const [ userFilters, setUserFilters ] = useState({})
+	const [ errors, setErrors ] = useState({})
+	// const [ searchData, setSearchData ] = useState([])
+
+	function handleChange(e) {
+		changeData(e, userFilters, setUserFilters, errors, setErrors)
+	}
+
+	useEffect(() => {
+		if (Object.keys(userFilters).length > 0 && JSON.stringify(userFilters) !== JSON.stringify(router.query)) {
+			Object.keys(userFilters).map(el => {
+                router.query[el] = userFilters[el]
+            })
+
+            router.push({
+                query: { ...router.query },
+            }, undefined, { scroll: false });
+
+			// ApiService.get(`${SEARCH_CONTENTS_ROUTE}`, {
+			// 	params: {...userFilters}
+			// }).then((response) => {
+			// 	setSearchData(response.data.data)
+			// })
+		}
+	}, [userFilters]);
+
+	function getModifiedQuery(name, value) {
+	    if (!name.includes('[]')) {
+	        return value
+	    }
+
+	    if (typeof value === 'string') {
+	        return [value]
+	    } else if (Array.isArray(value)) {
+	        return value
+	    }
+
+	    return []
+	}
+
+	useEffect(() => {
+	    const newQuery = {...query}
+
+	    let modifiedQuery = {}
+	    for (const [name, value] of Object.entries(newQuery)) {
+	        modifiedQuery[name] = getModifiedQuery(name, value)
+	    }
+
+	    setUserFilters({...userFilters, ...modifiedQuery})
+	}, []);
+
+
+	function removeFilter(key) {
+		if (key === 'all') {
+			setUserFilters({
+				...userFilters,
+				'categories[]': [],
+				search: ''
+			})
+		}
+		else {
+			changeData({
+				target: {
+					id: 'search',
+					name: 'search',
+					type: 'text',
+					checked: false,
+					value: '',
+					dataset: {}
+				}
+			}, userFilters, setUserFilters, errors, setErrors)
+		}
+	}
+
 
 	return (
 		<>
@@ -19,6 +98,9 @@ export default function Index(props) {
 
 			<SgSectionSearchSection
 				searchData={searchData}
+				userFilters={userFilters}
+				removeFilter={removeFilter}
+				handleChange={handleChange}
 			/>
 		</>
 	)
